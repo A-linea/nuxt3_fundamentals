07bef95ec1dab42363efd6b32ebf935ade5d1bf6
Eugene Dmitrievskiy <y.dmytriievskyi@liga.net>
2024-04-20 01:15:39 +0300

    Todolist with composition API

--- app.vue
+++ app.vue
@@ -6,10 +6,13 @@
     </div>
     <div class="flex">
       <div class="mt-3">
+        
         <img src="/todolist.jpg" class="w-1/3"/>
         <p>
           Photo by <a href="https://unsplash.com/@glenncarstenspeters?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Glenn Carstens-Peters</a> on <a href="https://unsplash.com/photos/person-writing-bucket-list-on-book-RLw-UC03Gwc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </p>
+        <span>Number of todos {{numbersOfTodos}}</span>
+        <span>Finished todos {{finishedTodos}}</span>
         <template v-if="todoList">
           <ul>
             <li v-for="todo in todoList" :key="todo.id">
@@ -22,21 +25,26 @@
   </div>
   </div>
 </template>
-<script>
-import {defineNuxtComponent} from "#app";
+<script setup>
+import { ref, reactive, computed } from 'vue'
 
-export default defineNuxtComponent({
-  data() {
-    return {
-      todoList: [],
-    }
-  },
-  methods: {
-    async fetchTodoList() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-      this.todoList = await response.json()
-    }
-  }
+let todoList = ref([])
+let newTodo = reactive({
+  title: '',
+  completed: false
 })
+const numbersOfTodos = computed(() => todoList.value.length)
+const finishedTodos = computed(() => todoList.value.filter(todo => todo.completed).length)
+async function fetchTodoList() {
+  // fetch('https://jsonplaceholder.typicode.com/todos')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     todoList.value = data
+  //   })
+  const resp  = await fetch('https://jsonplaceholder.typicode.com/todos')
+  todoList.value = await resp.json()
+}
+
+
 
 </script>
