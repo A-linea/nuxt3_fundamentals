bf3a430ac44c12383b98db8df28bea5310ea06ae
Eugene Dmitrievskiy <y.dmytriievskyi@liga.net>
2024-04-20 00:59:34 +0300

    Added app.vue

--- app.vue
+++ app.vue
@@ -2,10 +2,12 @@
   <div class="container m-auto">
     <div class="flex flex-col">
       <h1 class="text">Photo Gallery</h1>
-      <button class="bg-emerald-600" @click="fetchPhotoGallery" >Fetch Photos</button>
+      <button class="bg-emerald-600 p-3 mt-3" @click="fetchPhotoGallery" >Fetch Photos</button>
+      <p v-if="numPhotos > 0" class="mt-3">Number of photos: {{ numPhotos }}</p>
+      <p>Even photos is  {{evenPhotos.length}} ||| Odd photos is {{oddPhotos.length}}</p>
       <div class="grid grid-cols-3 gap-4">
         <div v-for="photo in photoGallery" :key="`photo-id-${photo.id}`" class="border">
-          <img :src="photo.url" alt="photo.title" class="w-full h-40 object-cover" />
+          <img :src="photo.url" :alt="photo.title" class="w-full h-40 object-cover" />
           <p>{{ photo.title }}</p>
         </div>
       </div>
@@ -25,6 +27,18 @@ export default defineNuxtComponent({
   data() {
     return {
       photoGallery: [],
+    }
+  },
+  
+  computed: {
+    numPhotos() {
+      return this.photoGallery.length
+    },
+    evenPhotos() {
+      return this.photoGallery.filter((photo, index) => index % 2 === 0)
+    },
+    oddPhotos() {
+      return this.photoGallery.filter((photo, index) => index % 2 !== 0)
     }
   },
   methods: {
