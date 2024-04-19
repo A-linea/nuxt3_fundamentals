f8f7b4f75fda635c9905ca0a8e4ca04156c8847f
Eugene Dmitrievskiy <y.dmytriievskyi@liga.net>
2024-04-20 00:59:16 +0300

    Photo albums

--- app.vue
+++ app.vue
@@ -1,40 +1,36 @@
 <template>
   <div class="container m-auto">
     <div class="flex flex-col">
-      <h1 class="columns-1 text-2xl text-blue-700">Nuxt 3 fundamentals</h1>
-      <button @click="fetchTodoList" class="mt-2 w-1/6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Fetch Todo List</button>
-    </div>
-    <div class="flex">
-      <div class="mt-3">
-        <img src="/todolist.jpg" class="w-1/3"/>
-        <p>
-          Photo by <a href="https://unsplash.com/@glenncarstenspeters?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Glenn Carstens-Peters</a> on <a href="https://unsplash.com/photos/person-writing-bucket-list-on-book-RLw-UC03Gwc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-        </p>
-        <template v-if="todoList">
-          <ul>
-            <li v-for="todo in todoList" :key="todo.id">
-              <strong class="w-[30px] inline-block">{{todo.id}}</strong>
-              <input class="mr-2" type="checkbox" :checked="todo.completed"/>
-              {{ todo.title }}</li>
-          </ul>
-        </template>
+      <h1 class="text">Photo Gallery</h1>
+      <button class="bg-emerald-600" @click="fetchPhotoGallery" >Fetch Photos</button>
+      <div class="grid grid-cols-3 gap-4">
+        <div v-for="photo in photoGallery" :key="`photo-id-${photo.id}`" class="border">
+          <img :src="photo.url" alt="photo.title" class="w-full h-40 object-cover" />
+          <p>{{ photo.title }}</p>
+        </div>
       </div>
+    </div>
+    <pre>
+      {{ photoGallery }}
+    </pre>
   </div>
-  </div>
+  <NuxtPage />
 </template>
+
+
 <script>
 import {defineNuxtComponent} from "#app";
 
 export default defineNuxtComponent({
   data() {
     return {
-      todoList: [],
+      photoGallery: [],
     }
   },
   methods: {
-    async fetchTodoList() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-      this.todoList = await response.json()
+    async fetchPhotoGallery() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/photos')
+      this.photoGallery = await response.json()
     }
   }
 })
