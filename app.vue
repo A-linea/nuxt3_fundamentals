19ed8f995bc37877aad3d8be5da0d636ce159c7b
Eugene Dmitrievskiy <y.dmytriievskyi@liga.net>
2024-04-20 22:53:25 +0300

    Routing basics

--- app.vue
+++ app.vue
@@ -1,15 +1,9 @@
 <template>
-  <div class="container pt-4 mx-auto">
-      <div class="flex flex-row justify-between">
-        <TodoViewer title="Todo List">
-        </TodoViewer>
-        <PhotoGallery title="Photo Gallery List" />
-      </div>
-  </div>
+  <TheNavbar />
+<NuxtPage />
 </template>
 <script setup>
-import TodoViewer from "~/components/TodoViewer.vue";
-import PhotoGallery from "~/components/PhotoGallery.vue";
+import TheNavbar from "~/components/UI/TheNavbar.vue";
 
 
 </script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TodoViewer from "~/components/TodoViewer.vue";
+import PhotoGallery from "~/components/PhotoGallery.vue";
+</script>
+
+<template>
+<h1>Home page</h1>
+  <div class="container pt-4 mx-auto">
+    <div class="flex flex-row justify-between">
+      <TodoViewer title="Todo List">
+      </TodoViewer>
+      <PhotoGallery title="Photo Gallery List" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
